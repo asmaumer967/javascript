9b37f74fc7ab625e4d7c9706c938675ab200f70b
asmaumer967 <asmaumer967@gmail.com>
2026-06-04 11:50:39 +0500

    first commit

--- script.js
+++ script.js
@@ -1,54 +1,14 @@
+var age = prompt("Enter you age");
 
-let num = 75;
-
-if (num > 100) {
-  console.log("Big number!");
-} else if (num > 50) {
-  console.log("Medium number");
-} else if (num > 10) {
-  console.log("Small number");
-} else {
-  console.log("Very small number");
+if(age >= 25 ){
+   console.log("Eligible for passport, driving license & CNIC");
 }
-
-let temp = 15;
-
-if (temp < 0) {
-  console.log("It's ice!");
-} else if (temp < 20) {
-  console.log("It's cold water");
-} else if (temp < 100) {
-  console.log("It's warm water");
-} else {
-  console.log("It's boiling!");
+else if (age >= 22){
+  console.log("Eligible for driving license , CNIC");
 }
-
-
-
-let day = "Monday";
-
-if (day === "Saturday") {
-  console.log("Weekend! Rest day.");
-} else if (day === "Sunday") {
-  console.log("Weekend! Fun day.");
-} else if (day === "Friday") {
-  console.log("Almost weekend!");
-} else {
-  console.log("It's a weekday. Study!");
+else if (age >= 18){
+  console.log("Eligible for CNIC");
 }
-
-
-
-let age   = 16;
-let money = 30;
-let price = 25;
-
-if (age >= 18 && money >= price) {
-  console.log("You can buy the game!");
-} else if (age < 18 && money >= price) {
-  console.log("You have money but too young.");
-} else if (age >= 18 && money < price) {
-  console.log("You're old enough but no money.");
-} else {
-  console.log("Too young AND no money!");
+if(num >= 10){
+  console.log("number is greater or equal to 10")
 }
